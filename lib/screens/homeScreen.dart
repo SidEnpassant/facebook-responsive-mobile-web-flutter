7ccc3bfa33e4e2863c3e5c3bb3e1e6d61234baec
SidEnpassant <siddhesdas2020@gmail.com>
2026-01-20 01:35:26 +0530

    DONE TILL 22:20

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,45 @@
+import 'package:facebookuiclone/config/palette.dart';
+import 'package:facebookuiclone/data/data.dart';
+import 'package:facebookuiclone/widgets/circlebutton.dart';
+import 'package:facebookuiclone/widgets/createPostContainer.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_mdi_icons/flutter_mdi_icons.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white, 
+            systemOverlayStyle: SystemUiOverlayStyle.dark,
+            title: Text(
+              'Facebook',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                color:Palette.facebookBlue,
+                letterSpacing: -1.2,
+              ),
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: [
+              CircleButton(icon: Icons.search, iconSize: 30.0, onPress: () => print('search')),
+              CircleButton(icon: Mdi.facebookMessenger, iconSize: 30.0, onPress: () => print('messenger')),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: CreatePostContainer(currentUser: currentUser),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/createPostContainer.dart
@@ -0,0 +1,57 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:facebookuiclone/models/models.dart';
+
+class CreatePostContainer extends StatelessWidget {
+  final currentUser;
+  const CreatePostContainer({super.key , required this.currentUser});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 20.0,
+                backgroundColor: Colors.grey[200],
+                backgroundImage: CachedNetworkImageProvider(
+                  currentUser.imageUrl,
+               )
+              ),
+              const SizedBox(width: 8.0,),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'What\'s on your mind?',
+                    
+                  ),
+                ),
+              )
+            ],
+          ),
+          const Divider(height: 10.0 , thickness: 0.5,),
+          Container(
+            height: 40.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton.icon(onPressed:() => print('Live') , icon: const Icon(Icons.videocam , color: Colors.red,),
+                label: const Text('Live')),
+                const VerticalDivider(width: 8.0,),
+                TextButton.icon(onPressed:() => print('Photo') , icon: const Icon(Icons.photo_library, color: Colors.green,),
+                label: const Text('Photo')),
+                const VerticalDivider(width: 8.0,),
+                TextButton.icon(onPressed:() => print('Room') , icon: const Icon(Icons.video_call , color: Colors.purpleAccent,),
+                label: const Text('Room')),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
